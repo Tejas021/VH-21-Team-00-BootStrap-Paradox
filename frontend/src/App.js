bab02725e5d4c9fe168b53198e10a22d9b124ec5
Dhruvil Mehta <dhruvilmehta2606@gmail.com>
2021-10-01 00:30:19 +0530

    Added Cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import BedsCard from './components/BedsCard/BedsCard';
+import CasesCard from './components/CasesCard/CasesCard';
+import YoutubeCard from './components/YoutubeCard/YoutubeCard';
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div className="">
+      {/* <CasesCard /> */}
+      {/* <BedsCard /> */}
+      {/* <YoutubeCard /> */}
     </div>
   );
 }
